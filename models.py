756e64d1e6c9d8c47f0d3d9904589fd768bd44e1
Yang Liu <liuyang12@focusmedia.cn>
2019-09-23 17:19:13 +0800

    update

--- models.py
+++ models.py
@@ -10,13 +10,15 @@ class FaceExpressionModel(nn.Module):
         super(FaceExpressionModel, self).__init__()
         resnet = models.resnet50(pretrained=True)
         # Remove linear and pool layers (since we're not doing classification)
-        modules = list(resnet.children())[:-1]
+        modules = list(resnet.children())[:-2]
         self.resnet = nn.Sequential(*modules)
+        self.avgpool = nn.AvgPool2d(kernel_size=2)
         self.fc = nn.Linear(2048, num_classes)
         self.softmax = nn.Softmax(dim=-1)
 
     def forward(self, images):
         x = self.resnet(images)  # [N, 2048, 1, 1]
+        x = self.avgpool(x)
         x = x.view(-1, 2048)  # [N, 2048]
         x = self.fc(x)
         out = self.softmax(x)
@@ -25,4 +27,4 @@ class FaceExpressionModel(nn.Module):
 
 if __name__ == "__main__":
     model = FaceExpressionModel().to(device)
-    summary(model, input_size=(3, 224, 224))
+    summary(model, input_size=(3, 48, 48))
