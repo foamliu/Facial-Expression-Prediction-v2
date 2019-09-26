bd36934f1e9758c27a17eed2ef1b7cc44ca2976d
Yang Liu <liuyang12@focusmedia.cn>
2019-09-26 14:56:45 +0800

    update

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import torch
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')  # sets device for model and PyTorch tensors
 
 # Model parameters
-im_size = 224
+im_size = 112
 num_channels = 3
 num_classes = 7
 
