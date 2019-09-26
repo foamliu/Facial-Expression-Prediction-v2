8155246f7bfcc03636ac7b6b0dab0872743c3ac3
Yang Liu <liuyang12@focusmedia.cn>
2019-09-26 16:20:23 +0800

    update

--- data_gen.py
+++ data_gen.py
@@ -16,7 +16,6 @@ from mtcnn.detector import detect_faces
 data_transforms = {
     'train': transforms.Compose([
         transforms.RandomHorizontalFlip(),
-        transforms.RandomRotation(degrees=10),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
     ]),
