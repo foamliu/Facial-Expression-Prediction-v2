2fba90319b763ccfdcbfc48fc9c85dcf9208123b
Yang Liu <liuyang12@focusmedia.cn>
2019-09-27 13:43:50 +0800

    udpate

--- export.py
+++ export.py
@@ -1,10 +1,28 @@
+import time
+
 import torch
 
+from models import FaceExpressionModel
 
 if __name__ == '__main__':
     checkpoint = 'BEST_checkpoint.tar'
+    print('loading {}...'.format(checkpoint))
+    start = time.time()
     checkpoint = torch.load(checkpoint)
+    print('elapsed {} sec'.format(time.time() - start))
     model = checkpoint['model']
-    # model.eval()
+    print(model)
+    print(type(model))
 
-    torch.save(model.state_dict(), 'facial_expression.pt')
+    # model.eval()
+    filename = 'facial_expression.pt'
+    print('saving {}...'.format(filename))
+    start = time.time()
+    torch.save(model.state_dict(), filename)
+    print('elapsed {} sec'.format(time.time() - start))
+
+    print('loading {}...'.format(filename))
+    start = time.time()
+    model = FaceExpressionModel()
+    model.load_state_dict(torch.load(filename))
+    print('elapsed {} sec'.format(time.time() - start))
